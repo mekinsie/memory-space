d8845465f3b5cce31b7f2976439f21f4b9304088
Lo-GR <diamondintheroth@gmail.com>
2021-05-03 15:17:05 -0700

    create project
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <MemoryControl />
+    </React.Fragment >
+  );
+}
+
+export default App;
