c5e5b6c254fd079050e7c4fc80c847ca2f9fbc57
Lo-GR <diamondintheroth@gmail.com>
2021-05-04 13:57:32 -0700

    commit before merge
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function ReusableForm(props) {
   return (
     <React.Fragment>
-      <form onSubmit={props.formSubmissionHandler}>
+      <form id="form" onSubmit={props.formSubmissionHandler}>
         <input
           type='text'
           name='name'
