ae02d7a97bab907d1691c1122026682077c1d04f
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-05-06 13:19:35 -0700

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer(){
+  return(
+<div id="home-footer" class="center-text">
+  <p>©2021 Memory Space</p>
+  <p>
+    <a href="https://github.com/mekinsie">Mekinsie Callahan</a> |
+    <a href="https://github.com/Lo-GR">Logan Roth</a>
+  </p>
+</div>
+  )
+}
+
+export default Footer;
