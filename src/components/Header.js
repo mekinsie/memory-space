475f505219f9aa905a00311cbd80835454c5ccb7
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-05-06 11:03:38 -0700

    Add description of site in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Header(){
   return (
+    <>
     <h1>Memory Space</h1>
+    <span class="center"><p>This is a space where anyone can post their memories. We ask that you only edit or delete your own memory.</p></span>
+    </>
   );
 }
 
